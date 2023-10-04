9f9567b53ee2984d7e162b1a20b500deba98a74f
rayhankinan <rayhankinan@gmail.com>
2023-10-04 13:07:04 +0700

    Initialize wrapper

---
+++ src/+wrappers/GrayscaleImageWrapper.m
@@ -0,0 +1,40 @@
+classdef GrayscaleImageWrapper < wrappers.BaseImageWrapper
+    methods
+        % Constructor
+        function obj = GrayscaleImageWrapper(imageData)
+            arguments
+                imageData uint8
+            end
+            
+            obj.ImageData = imageData;
+            obj.Type = 'grayscale';
+        end
+        
+        % Check if Image is empty
+        function isEmpty = IsEmpty(obj)
+            arguments
+                obj wrappers.GrayscaleImageWrapper
+            end
+            
+            isEmpty = isempty(obj.ImageData);
+        end
+        
+        % Get Image Data
+        function data = GetImageData(obj)
+            arguments
+                obj wrappers.GrayscaleImageWrapper
+            end
+            
+            data = obj.ImageData;
+        end
+        
+        % Get Type
+        function type = GetType(obj)
+            arguments
+                obj wrappers.GrayscaleImageWrapper
+            end
+            
+            type = obj.Type;
+        end
+    end
+end
