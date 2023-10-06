bf2d291ef81e48fda31335a6cd7b4a733f436e46
Andhika Arta Aryanto <71170262+dhikaarta@users.noreply.github.com>
2023-10-06 21:59:11 +0700

    try fix

--- src/+utils/Wiener.m
+++ src/+utils/Wiener.m
@@ -1,13 +1,13 @@
 classdef Wiener
     methods (Static)
-        function deconvolved_image = myWienerDeconvolution(blurred_image, psf, K)
+        function deconvolved_image = WienerDeconvolution(blurred_image, psf, K)
             % blurred_image: image blur
             % psf: Point Spread Function
             % noise : Noise additive
             
             % ubah ke double precision
-            blurred_image = double(blurred_image);
-            psf = double(psf);
+            blurred_image = im2double(blurred_image);
+            psf = im2double(psf);
             
             % fft dari psf, dengan dimensi disamakan dengan image
             F_psf = fft2(psf, size(blurred_image, 1), size(blurred_image, 2));
@@ -19,14 +19,14 @@ classdef Wiener
             F_blurred = fft2(blurred_image);
             
             % mengalikan penapis wiener dengan foto
-            F_deconvolved = Wiener_filter .* F_blurred;
+            F_deconvolved =  F_blurred .* Wiener_filter;
             
             % inverse
             deconvolved_image = ifft2(F_deconvolved);
             
             % memastikan angka real dan mengembalikan ke tipe int
             deconvolved_image = real(deconvolved_image);
-            deconvolved_image = uint8(deconvolved_image);
+            deconvolved_image = im2uint8(deconvolved_image);
             
             % clip values diluar 0 - 255
             deconvolved_image(deconvolved_image < 0) = 0;
